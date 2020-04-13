a742f1da4bf5993d3812b661abf0a49d59175b12
letrananhvu <vule5726@gmail.com>
2020-04-13 18:53:41 +0300

    done

---
+++ src/BubbleButton.js
@@ -0,0 +1,31 @@
+import React, {useRef} from "react";
+import './BubbleButton.scss';
+
+const BubbleButton = ({color, children, onClick, isDisabled, style, size, ...attr}) => {
+    const refDot = useRef(null);
+    const refDiv = useRef(null);
+    const hoverEffect = (e) => {
+        let statics = refDiv.current.getBoundingClientRect();
+        let dx = e.clientX - statics.left;
+        let dy = e.clientY - statics.top;
+        refDot.current.style.clipPath = `circle(150% at ${dx}px ${dy}px)`;
+    };
+
+    const releaseEffect = (e) => {
+        let statics = refDiv.current.getBoundingClientRect();
+        let dx = e.clientX - statics.left;
+        let dy = e.clientY - statics.top;
+        refDot.current.style.clipPath = `circle(0% at ${dx}px ${dy}px)`;
+    };
+    return (
+        <div ref={refDiv} onMouseOut={releaseEffect} onMouseEnter={hoverEffect} className='' {...attr} style={style}
+             onClick={onClick}
+             className={`bubble-button-div ${size} ${isDisabled ? 'disabled' : ''} ${color ? color : 'blue'}`}>
+            <div ref={refDot} className='dot'/>
+            <button className='bubble-button'>
+                {children}</button>
+        </div>
+    )
+};
+
+export default BubbleButton;
